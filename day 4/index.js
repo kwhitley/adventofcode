4c609bebc3936f3a359e51fe03ddc62347e78fff
Kevin Whitley <krwhitley@gmail.com>
2021-12-04 17:33:29 -0600

    readability: 1, lines: 0

--- day 4/index.js
+++ day 4/index.js
@@ -3,7 +3,11 @@ import { sum } from 'supergeneric'
 export const play = (input, part2) => {
   let [draws, ...boards] = input.split('\n\n')
   draws = draws.split(',').map(Number)
-  boards = boards.map(b => [b.trim().replace(/\s+/gi, ' ').split(' ').map(Number), Array(5).fill(0), Array(5).fill(0)])
+  boards = boards.map(b => [
+    b.trim().replace(/\s+/gi, ' ').split(' ').map(Number),
+    Array(5).fill(0),
+    Array(5).fill(0),
+  ])
 
   for (const draw of draws) {
     for (const [board, rows, cols] of boards) {
