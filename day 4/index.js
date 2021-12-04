4ac8d6fbf314be0bf293ad3ae22744a96fd95dac
Kevin Whitley <krwhitley@gmail.com>
2021-12-04 17:31:17 -0600

    even shorter

--- day 4/index.js
+++ day 4/index.js
@@ -3,25 +3,18 @@ import { sum } from 'supergeneric'
 export const play = (input, part2) => {
   let [draws, ...boards] = input.split('\n\n')
   draws = draws.split(',').map(Number)
-  boards = boards.map(b => b.trim().replace(/\s+/gi, ' ').split(' ').map(Number))
-
-  for (const board of boards) {
-    board.plays = {
-      rows: Array(5).fill(0),
-      cols: Array(5).fill(0),
-    }
-  }
+  boards = boards.map(b => [b.trim().replace(/\s+/gi, ' ').split(' ').map(Number), Array(5).fill(0), Array(5).fill(0)])
 
   for (const draw of draws) {
-    for (const board of boards) {
+    for (const [board, rows, cols] of boards) {
       const index = board.indexOf(draw)
       const [row, col] = [Math.floor(index / 5), index % 5]
       board[index] = null
-      board.plays.rows[row]++
-      board.plays.cols[col]++
+      rows[row]++
+      cols[col]++
 
-      if (board.plays.cols[col] > 4 || board.plays.rows[row] > 4) {
-        boards = boards.filter(b => b !== board)
+      if (rows[row] > 4 || cols[col] > 4) {
+        boards = boards.filter(b => b[0] !== board)
 
         if (!part2 || !boards.length)
           return [draw, sum(board)]
