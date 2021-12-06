71995c7186466d56b0cf49afeb7d9a90b9c2fe9f
Kevin Whitley <krwhitley@gmail.com>
2021-12-05 23:47:41 -0600

    brevity

--- day 6/index.js
+++ day 6/index.js
@@ -2,18 +2,12 @@ import { sum } from 'supergeneric'
 
 export const grow = (population = [], days = 80, ages = Array(9).fill(0)) => {
   for (const p of population) {
-    ages[p]++
+    Number(p) && ages[p]++
   }
 
   for (let d=days; d>0; d--) {
-    const respawn = ages[0]
-
-    for (let age=1; age<=8; age++) {
-      ages[age-1] = ages[age]
-      ages[age] = 0
-    }
-
-    ages[8] += respawn
+    const respawn = ages.shift()
+    ages.push(respawn)
     ages[6] += respawn
   }
 
