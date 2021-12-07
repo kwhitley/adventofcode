6b379e4c52e3878d440bb8ef8353a83291fe16e2
Kevin Whitley <krwhitley@gmail.com>
2021-12-07 00:56:07 -0600

    day 7 cleanup

--- day 7/index.js
+++ day 7/index.js
@@ -1,9 +1,7 @@
-import { average, sum, median, ascending, round } from 'supergeneric'
+import { average, median, sum } from 'supergeneric'
 
 export const reposition = (positions) => {
   positions = positions.split(',').map(Number)
-
-  const mean = average(positions)
   const med = median(positions)
   const fuelFromMedian = sum(positions.map(p => Math.abs(p - med)))
 
@@ -13,11 +11,14 @@ export const reposition = (positions) => {
 export const reposition2 = (positions) => {
   positions = positions.split(',').map(Number)
 
-  const ranges = Array(1500).fill(0).map((v, i) => i)
-  const fuelCalcs = ranges.reduce((acc, r) => Number.isFinite((acc[r] = sum(ranges.slice(0, r+1)))) && acc, {})
+  let fuel = Array(1500)
+              .fill(0)
+              .map((v, i) => i)
+              .reduce((acc, i) => (acc[i] = acc[i-1]+i) && acc || acc, { [-1]: 0 })
+
   const target = average(positions)|0
-  const bestFuelA = sum(positions.map(p => fuelCalcs[Math.abs(p - target)]))
-  const bestFuelB = sum(positions.map(p => fuelCalcs[Math.abs(p - target - 1)]))
+  const bestFuelA = sum(positions.map(p => fuel[Math.abs(p - target)]))
+  const bestFuelB = sum(positions.map(p => fuel[Math.abs(p - target - 1)]))
 
   return Math.min(bestFuelA, bestFuelB)
 }
