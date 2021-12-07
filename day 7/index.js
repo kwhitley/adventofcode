403fc7f85aac9b6124e2695ce77b71f1fa9101e2
Kevin Whitley <krwhitley@gmail.com>
2021-12-07 00:43:09 -0600

    cleanup day 7

--- day 7/index.js
+++ day 7/index.js
@@ -5,8 +5,6 @@ export const reposition = (positions) => {
 
   const mean = average(positions)
   const med = median(positions)
-
-
   const fuelFromMedian = sum(positions.map(p => Math.abs(p - med)))
 
   return [ med, fuelFromMedian ]
@@ -17,22 +15,15 @@ export const reposition2 = (positions) => {
 
   const ranges = Array(1500).fill(0).map((v, i) => i)
   const fuelCalcs = ranges.reduce((acc, r) => Number.isFinite((acc[r] = sum(ranges.slice(0, r+1)))) && acc, {})
-
   let target = average(positions)|0
-  let bestFuelA = sum(positions.map(p => fuelCalcs[Math.abs(p - target)]))
-  let bestFuelB = sum(positions.map(p => fuelCalcs[Math.abs(p - target - 1)]))
-  let bestFuel = Math.min(bestFuelA, bestFuelB)
-  const direction = bestFuelA < bestFuelB ? -1 : 1
+  let bestFuel = sum(positions.map(p => fuelCalcs[Math.abs(p - target)]))
+  const direction = sum(positions.map(p => fuelCalcs[Math.abs(p - target - 1)])) < bestFuel ? 1 : -1
 
   while (true) {
+    let newFuel = sum(positions.map(p => fuelCalcs[Math.abs(p - target - direction)]))
+    if (newFuel >= bestFuel) break
+    bestFuel = newFuel
     target += direction
-    let newFuel = sum(positions.map(p => fuelCalcs[Math.abs(p - target)]))
-
-    if (newFuel < bestFuel) {
-      bestFuel = newFuel
-    } else {
-      break
-    }
   }
 
   return [ target, bestFuel ]
