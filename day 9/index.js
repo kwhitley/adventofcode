3417b53c51d23995febbd8993cb8e4a342d4ad53
Kevin Whitley <krwhitley@gmail.com>
2021-12-09 12:43:11 -0600

    min day 9

--- day 9/index.js
+++ day 9/index.js
@@ -2,30 +2,25 @@ import { ascending, onlyNumbers } from 'supergeneric'
 
 export const getRisk = (input, part2, risk = 0, basins = []) => {
   const rowLength = input.split('\n')[0].length
-  const grid = input.split('\n').map(row => row.split('').map(Number))
+  const g = input.split('\n').map(row => row.split('').map(Number))
 
-  for (let r=0; r<grid.length; r++) {
-    for (let c=0; c<grid[r].length; c++) {
-      const adjacent = [
-        grid[r-1]?.[c],
-        grid[r]?.[c-1],
-        grid[r]?.[c+1],
-        grid[r+1]?.[c],
-      ]
+  for (let r=0; r<g.length; r++) {
+    for (let c=0; c<g[r].length; c++) {
+      const adjacent = [g[r-1]?.[c], g[r]?.[c-1], g[r]?.[c+1], g[r+1]?.[c] ]
 
-      if (Math.min(...onlyNumbers(adjacent)) > grid[r][c]) {
-        risk += (grid[r][c] + 1)
+      if (Math.min(...onlyNumbers(adjacent)) > g[r][c]) {
+        risk += (g[r][c] + 1)
         const scanned = []
         const basin = []
 
         const getBasin = (r, c, coordinates = `${r},${c}`) => {
-          if (scanned.includes(coordinates) || grid[r][c] === 9) return undefined
+          if (scanned.includes(coordinates) || g[r][c] === 9) return undefined
           scanned.push(coordinates)
           basin.push(coordinates)
           r > 0 && getBasin(r-1, c)
-          r < grid.length - 1 && getBasin(r+1, c)
+          r < g.length - 1 && getBasin(r+1, c)
           c > 0 && getBasin(r, c-1)
-          c < grid[0].length - 1 && getBasin(r, c+1)
+          c < g[0].length - 1 && getBasin(r, c+1)
         }
 
         getBasin(r, c)
