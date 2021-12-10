c3050ac942db784e30f1846e408f749c7875109c
Kevin Whitley <krwhitley@gmail.com>
2021-12-10 00:00:51 -0600

    day 10 cleanup

--- day 10/index.js
+++ day 10/index.js
@@ -1,20 +1,18 @@
-import { ascending, onlyNumbers, median } from 'supergeneric'
+import { median } from 'supergeneric'
 
-export const scan = (input, part2, score = 0) => {
-  let autocompletes = []
-
+export const scan = (input, part2, score = 0, autocompletes = []) => {
   outer: for (const line of input.split('\n')) {
     const stack = []
 
     for (const c of line) {
       const lut = {
-        '{': '}',
-        '[': ']',
-        '<': '>',
-        '(': ')',
+        '(': [')', 3],
+        '[': [']', 57],
+        '{': ['}', 1197],
+        '<': ['>', 25137],
       }
 
-      if (stack[0] === c) {
+      if (stack[0]?.[0] === c) {
         stack.shift()
         continue
       }
@@ -22,21 +20,12 @@ export const scan = (input, part2, score = 0) => {
       if (lut[c]) {
         stack.unshift(lut[c])
       } else if (stack.length) {
-        const scores = {
-          ')': 3,
-          ']': 57,
-          '}': 1197,
-          '>': 25137,
-        }
-        score += scores[c]
-
+        score += Object.values(lut).find(i => i[0] === c)[1]
         continue outer
       }
     }
 
-    if (part2) {
-      autocompletes.push(stack.reduce((acc, c) => acc * 5 + ')]}>'.indexOf(c) + 1, 0))
-    }
+    autocompletes.push(stack.reduce((acc, c) => acc * 5 + ')]}>'.indexOf(c[0]) + 1, 0))
   }
 
   return part2 ? median(autocompletes) : score
