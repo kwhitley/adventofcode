556cccd339b2a9ee87c19011a0b9b9caacf7218c
Kevin Whitley <krwhitley@gmail.com>
2021-12-12 00:34:19 -0600

    day 12 final

--- day 12/index.js
+++ day 12/index.js
@@ -1,30 +1,29 @@
 export const run = (input, part2, paths = input.split('\n').map(l => l.split('-'))) => {
-  const validPaths = new Set()
-  const map = {}
+  const valid = new Set()
+  const branches = {}
 
   for (const [from, to] of paths) {
-    map[from] = map[from] || []
-    map[to] = map[to] || []
-    to !== 'start' && map[from].push(to)
-    from !== 'start' && map[to].push(from)
+    branches[from] = branches[from] || []
+    branches[to] = branches[to] || []
+    to !== 'start' && branches[from].push(to)
+    from !== 'start' && branches[to].push(from)
   }
 
   const buildPath = (pathSoFar, duplicateVisit) => {
-    const lastPath = pathSoFar[0]
-    const isSmallCave = lastPath.toLowerCase() === lastPath
+    const prev = pathSoFar[0]
 
-    if (lastPath === 'end') {
-      return validPaths.add(pathSoFar)
-    } else if (part2 && pathSoFar.slice(1).includes(lastPath) && lastPath.toLowerCase() === lastPath) {
+    if (prev === 'end') {
+      return valid.add(pathSoFar)
+    } else if (part2 && pathSoFar.slice(1).includes(prev) && prev.toLowerCase() === prev) {
       duplicateVisit = true
     }
 
-    map[lastPath]
+    branches[prev]
       .filter(b => b.toUpperCase() === b || !pathSoFar.includes(b) || (part2 && !duplicateVisit))
       .map(b => buildPath([b, ...pathSoFar], duplicateVisit))
   }
 
   buildPath(['start'])
 
-  return validPaths.size
+  return valid.size
 }
