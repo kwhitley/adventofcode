7ec16553cc6edf18f044db5708325903b850a591
Kevin Whitley <krwhitley@gmail.com>
2021-12-13 03:00:15 -0600

    cleanup day 13

--- day 13/index.js
+++ day 13/index.js
@@ -3,31 +3,18 @@ import { numbers, sum } from 'supergeneric'
 export const run = (input, maxInstructions = 1, part2) => {
   const [dots, instructions] = input
                                 .split('\n\n')
-                                .map((set, i) =>
-                                  set
-                                    .split('\n')
-                                    .map(line => line.replace(/^.*\s/, '').split(i ? '=' : ',').map(numbers))
-                                )
+                                .map((set, i) => set.split('\n').map(line => line.replace(/^.*\s/, '').split(i ? '=' : ',').map(numbers)))
   const maxY = Math.max(...dots.map(d => d[1]))
   const maxX = Math.max(...dots.map(d => d[0]))
   let grid = Array(maxY+(part2?2:1)).fill(0).map(() => Array(maxX+1).fill(0)) // solved final by shifting to maxY+2... WTF.
+  dots.forEach(([x, y]) => grid[y][x] = 1)
 
-  for (const [x, y] of dots) {
-    grid[y][x] = 1
-  }
+  const fold = (grid, axis, at) => {
+    const gridB = axis === 'y'
+                ? grid.splice(at, grid.length).reverse()
+                : grid.map(line => line.splice(at, grid.length).reverse())
 
-  const fold = (grid, axis, at, gridB) => {
-    gridB = axis === 'y'
-          ? grid.splice(at, grid.length).reverse()
-          : grid.map(line => line.splice(at, grid.length).reverse())
-
-    for (let y=0; y<grid.length; y++) {
-      for (let x=0; x<grid[y].length; x++) {
-        grid[y][x] = grid[y][x] || gridB[y][x]
-      }
-    }
-
-    return grid
+    return grid.map((line, y) => line.map((v, x) => v || gridB[y][x]))
   }
 
   for (const [axis, at] of instructions) {
