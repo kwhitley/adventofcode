290a53a75b87b31b3848ddd4d638fa7c153750d9
Kevin Whitley <krwhitley@gmail.com>
2021-12-14 00:32:29 -0600

    day 14 complete... now for cleanup

--- day 14/index.js
+++ day 14/index.js
@@ -1,44 +1,33 @@
 import { numbers, sum } from 'supergeneric'
 
-export const run = (input, steps = 1, part2) => {
+export const run = (input, steps = 1, c) => {
   let [base, rules] = input.split('\n\n').map((set, i) => i ? set.split('\n').map(l => l.split(' -> ')) : set)
 
   let template = {}
 
   rules = rules.reduce((acc, [pair, c]) => ({ ...acc, [pair]: c }), {})
 
-  // console.log({ rules })
-
   for (let i=0; i<base.length-1; i++) {
     template[base.slice(i, i+2)] = (template[base.slice(i, i+2)] || 0) + 1
   }
-
-  // console.log({ template })
 
   for (let step=0; step<steps; step++) {
     const update = { ...template }
 
     for (const [pair, count] of Object.entries(template)) {
-      // console.log('step', step+1, 'considering pair', pair)
-      const c = rules[pair]
-
-      if (c) {
+      if (c = rules[pair]) {
         const [left, right] = pair.split('')
         update[pair] = (update[pair] || 0) - count
         update[left+c] = (update[left+c] || 0) + count
         update[c+right] = (update[c+right] || 0) + count
-        // update[]
-        // console.log('found matching rule, injecting', c, 'between', left, 'and', right)
       }
     }
 
     template = update
-    // console.log('template after step', step, 'is', template)
   }
 
   let freq = Object.entries(template).filter(([pair, c]) => c).reduce((acc, [pair, c]) => {
     const [left, right] = pair.split('')
-
     acc[left] = (acc[left] || 0) + c
     acc[right] = (acc[right] || 0) + c
 
@@ -48,14 +37,4 @@ export const run = (input, steps = 1, part2) => {
   freq = Object.entries(freq).map(([c, n]) => [c, Math.ceil(n/2)]).sort((a, b) => a[1] > b[1] ? 1 : -1)
 
   return freq.pop()[1] - freq.shift()[1]
-
-  // const sorted = Object.entries(template.split('').reduce((acc, c) => {
-  //   acc[c] = (acc[c] || 0) + 1
-
-  //   return acc
-  // }, {})).sort((a, b) => a[1] > b[1] ? 1 : -1)
-
-  // console.log('sorted after', steps, 'steps', sorted)
-
-  // return //template, sorted.pop()[1] - sorted.shift()[1]]
 }
