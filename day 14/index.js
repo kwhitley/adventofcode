70936e0ef2ac964ff2cb69e2b695c70c3496fa57
Kevin Whitley <krwhitley@gmail.com>
2021-12-14 00:29:30 -0600

    freaking finally

--- day 14/index.js
+++ day 14/index.js
@@ -13,37 +13,41 @@ export const run = (input, steps = 1, part2) => {
     template[base.slice(i, i+2)] = (template[base.slice(i, i+2)] || 0) + 1
   }
 
-  console.log({ template })
+  // console.log({ template })
 
   for (let step=0; step<steps; step++) {
-    const update = {}
+    const update = { ...template }
 
     for (const [pair, count] of Object.entries(template)) {
-      console.log('step', step+1, 'considering pair', pair)
+      // console.log('step', step+1, 'considering pair', pair)
       const c = rules[pair]
 
       if (c) {
         const [left, right] = pair.split('')
-        update[pair] = (update[pair] || 0) - template[pair]
-        update[left+c] = (update[left+c] || 0) + 1
-        update[c+right] = (update[c+right] || 0) + 1
+        update[pair] = (update[pair] || 0) - count
+        update[left+c] = (update[left+c] || 0) + count
+        update[c+right] = (update[c+right] || 0) + count
         // update[]
-        console.log('found matching rule, injecting', c, 'between', left, 'and', right)
+        // console.log('found matching rule, injecting', c, 'between', left, 'and', right)
       }
     }
 
-    console.log('template after step 1', template)
-    console.log('update after step 1', update)
-
-    for (const [pair, count] of Object.entries(update)) {
-      template[pair] = (template[pair] || 0) + count
-    }
-
-    console.log('after merge', template)
-
+    template = update
+    // console.log('template after step', step, 'is', template)
   }
 
-  console.log({ template })
+  let freq = Object.entries(template).filter(([pair, c]) => c).reduce((acc, [pair, c]) => {
+    const [left, right] = pair.split('')
+
+    acc[left] = (acc[left] || 0) + c
+    acc[right] = (acc[right] || 0) + c
+
+    return acc
+  }, {})
+
+  freq = Object.entries(freq).map(([c, n]) => [c, Math.ceil(n/2)]).sort((a, b) => a[1] > b[1] ? 1 : -1)
+
+  return freq.pop()[1] - freq.shift()[1]
 
   // const sorted = Object.entries(template.split('').reduce((acc, c) => {
   //   acc[c] = (acc[c] || 0) + 1
@@ -53,5 +57,5 @@ export const run = (input, steps = 1, part2) => {
 
   // console.log('sorted after', steps, 'steps', sorted)
 
-  return //template, sorted.pop()[1] - sorted.shift()[1]]
+  // return //template, sorted.pop()[1] - sorted.shift()[1]]
 }
