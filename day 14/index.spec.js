c81f93c78c3dd45139c720f9dbf69e3831179341
Kevin Whitley <krwhitley@gmail.com>
2021-12-14 00:39:36 -0600

    cleaned up spec file

--- day 14/index.spec.js
+++ day 14/index.spec.js
@@ -24,12 +24,7 @@ CN -> C`
 describe('DAY 14', () => {
   describe('part 1', () => {
     it('should do a thing', () => {
-      // expect(challenge.run(sample1, 1)[0]).toEqual('NCNBCHB')
-      // expect(challenge.run(sample1, 2)[0]).toEqual('NBCCNBBBCBHCB')
-      // expect(challenge.run(sample1, 3)[0]).toEqual('NBBBCNCCNBBNBNBBCHBHHBCHB')
-      // expect(challenge.run(sample1, 4)[0]).toEqual('NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB')
       expect(challenge.run(sample1, 10)).toEqual(1588)
-      // expect(challenge.run(sample1, 10)).toEqual(1588)
     })
 
     it('FINAL', () => {
