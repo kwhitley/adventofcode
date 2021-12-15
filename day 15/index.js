98586f16f290d1229f84fea279a6d8b76d310e93
Kevin Whitley <krwhitley@gmail.com>
2021-12-15 17:35:02 -0600

    day 15 cleanup

--- day 15/index.js
+++ day 15/index.js
@@ -2,25 +2,10 @@ import { numbers } from 'supergeneric'
 
 export const run = (input, part2) => {
   let grid = input.split('\n').map(line => line.split('').map(numbers))
-
-  if (part2) {
-    let width = grid[0].length
-    let height = grid.length
-
-    for (let y=0; y<5*height; y++) {
-      for (let x=0; x<5*width; x++) {
-        const risk = (y/height|0) + (x/width|0);
-        grid[y] = grid[y] || []
-        grid[y][x] = grid[y][x] || (grid[y%height][x%width] + risk)
-        if (grid[y][x] > 9) {
-          grid[y][x] = grid[y][x] % 9
-        }
-      }
-    }
-  }
-
-  const bottom = grid.length - 1
-  const right = grid[0].length - 1
+  let width = grid[0].length
+  let height = grid.length
+  let bottom = height * (part2 ? 5 : 1) - 1
+  let right = width * (part2 ? 5 : 1) - 1
 
   const cost = {}
   const queue = [[0, 0, 0]]
@@ -39,8 +24,14 @@ export const run = (input, part2) => {
         const newX = x + dx
         const newY = y + dy
 
-        if (newY >= 0 && newY <= bottom && newX <= right && newX >= 0)
-          queue.push([grid[newY][newX] + c, newY, newX])
+        if (newY >= 0 && newY <= bottom && newX <= right && newX >= 0) {
+          let weight = (grid[newY % height][newX % width] + (newY/height|0) + (newX/width|0))
+          if (weight > 9) {
+            weight = weight % 9
+          }
+          queue.push([weight + c, newY, newX])
+        }
+
       }
 
       queue.sort((a, b) => a[0] > b[0] ? 1 : -1)
