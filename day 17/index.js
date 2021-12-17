0048e48568c4352232aceca31651839c99603259
Kevin Whitley <krwhitley@gmail.com>
2021-12-17 11:07:24 -0600

    brute force approach

--- day 17/index.js
+++ day 17/index.js
@@ -6,12 +6,11 @@ export const run = (input, part2) => {
   const target = input.split('=').slice(1).map(pair => pair.split('..').map(c => Number(c.replace(/[^\d-]/g, ''))))
   const inBoundsX = between(...target[0])
   const inBoundsY = between(...target[1])
-  const pastTarget = (x, y) => x > target[0][1]
   const inBounds = (x, y) => inBoundsX(x) && inBoundsY(y)
+  let hits = [], hit
 
   const play = (v, target) => {
-    let pos = [0, 0]
-    let ticks = 0
+    const pos = [0, 0] // start at 0,0
     const inPlay = ([x, y]) => x < target[0][1] && y > target[1][0]
     let maxY = -Infinity
     const advance = (p, v) => {
@@ -23,42 +22,25 @@ export const run = (input, part2) => {
     }
 
     while (inPlay(pos)) {
-      ticks++
       advance(pos, v)
-      if (inBounds(...pos)) return [true, false, maxY]
+      if (inBounds(...pos)) return [maxY]
     }
 
-    return [false, pastTarget]
+    return false
   }
 
-  const queue = [[5, 7]]
-  const visited = new Set()
-  const hits = []
-  let tries = 0
+  let ticks = 0
 
-  while (queue.length && tries < 1000000) {
-    tries++
-    const [x, y] = queue.shift()
-    const key = `${x},${y}`
-
-    if (!visited.has(key)) {
-      visited.add(key)
-      // console.log('trying velocity', [x, y])
-
-      const [hit, pastX, maxY] = play([x, y], target)
-
-      if (hit) {
-        hits.push([maxY, x, y])
-      }
-
-      for (const [dx, dy] of [[0, 1], [1, 0], [0, -1]]) {
-        const newX = x + dx
-        const newY = y + dy
-
-        queue.push([newX, newY])
+  forX: for (let xv=0; xv<target[0][1]*1.1; xv++) {
+    forY: for (let yv=-200; yv<200; yv++) {
+      ticks++
+      if (hit = play([xv, yv], target)) {
+        hits.push(hit)
       }
     }
   }
+
+  console.log('scanned', ticks, 'velocities')
 
   return part2 ? hits.length : Math.max(...hits.map(h => h[0]))
 }
