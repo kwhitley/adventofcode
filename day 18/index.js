e5902f2ac65ad736f50f4ca8666e266b80ea6577
Kevin Whitley <krwhitley@gmail.com>
2021-12-18 23:23:38 -0600

    solved day 18

--- day 18/index.js
+++ day 18/index.js
@@ -1,67 +1,198 @@
-import { sum } from 'supergeneric'
+import { sum, round } from 'supergeneric'
 
-// const between = (a, b) => c => c >= Math.min(a, b) && c <= Math.max(a, b)
-
-export const reduce = (input, part2) => {
-  const parsed = typeof input === 'string' ? input.split('\n').map(line => JSON.parse(line)) : input
-
-
-
-  // utility function to return flattened tree
-  const print = node => typeof node.value === 'number' ? node.value : [print(node.left), print(node.right)]
-
-  // creates nodes
-  const node = (value, parent, n = { parent }) => {
-    if (typeof value === 'number') {
-      n.value = value
-    } else {
-      n.left = node(value[0], n)
-      n.right = node(value[1], n)
-    }
-
-    return n
+// creates nodes
+const createNode = (value, parent, depth = 1, n = { parent, depth }) => {
+  if (typeof value === 'number') {
+    n.value = value
+  } else {
+    n.left = createNode(value[0], n, depth + 1)
+    n.right = createNode(value[1], n, depth + 1)
   }
 
+  // console.log('creating node', print(n), 'at depth', n.depth)
 
-  const tree = node(parsed)
+  return n
+}
 
-  // console.log({ parsed })
+// utility function to return flattened root
+const flatten = node => typeof node.value === 'number' ? node.value : [flatten(node.left), flatten(node.right)]
 
-  console.log('flat tree', JSON.stringify(print(tree), null))
+const nodes = (node, set = []) => {
+  if (node) {
+    set.push(node)
+    nodes(node.left, set)
+    nodes(node.right, set)
+  }
 
-  // const target = input.split('=').slice(1).map(pair => pair.split('..').map(c => Number(c.replace(/[^\d-]/g, ''))))
-  // const inBoundsX = between(...target[0])
-  // const inBoundsY = between(...target[1])
-  // const inBounds = (x, y) => inBoundsX(x) && inBoundsY(y)
-  // let hits = [], hit
+  return set
+}
 
-  // const play = (v, target) => {
-  //   const pos = [0, 0] // start at 0,0
-  //   const inPlay = ([x, y]) => x < target[0][1] && y > target[1][0]
-  //   let maxY = -Infinity
-  //   const advance = (p, v) => {
-  //     p[0] += v[0]
-  //     p[1] += v[1]
-  //     v[1] -= 1
-  //     v[0] = Math.max(0, v[0]-1)
-  //     maxY = Math.max(maxY, p[1])
-  //   }
+const values = (node, set = []) => {
+  if (typeof node.value === 'number') {
+    set.push(node)
+  } else {
+    values(node.left, set)
+    values(node.right, set)
+  }
 
-  //   while (inPlay(pos)) {
-  //     advance(pos, v)
-  //     if (inBounds(...pos)) return maxY
-  //   }
+  return set
+}
 
-  //   return false
-  // }
+export const magnitude = node => {
+  if (typeof node.value === 'number') return node.value
 
-  // forX: for (let xv=0; xv<target[0][1]*1.01; xv++) {
-  //   forY: for (let yv=target[1][0]; yv<Math.abs(target[1][0]); yv++) {
-  //     if ((hit = play([xv, yv], target)) !== false) {
-  //       hits.push(hit)
-  //     }
-  //   }
-  // }
+  return 3 * magnitude(node.left) + 2 * magnitude(node.right)
+}
 
-  return parsed
+// explodes stuff
+const explode = (node, flat) => {
+  if (!node) return false
+
+  // console.log('testing for explosion with depth', node.depth, 'and type', typeof node.value, node)
+  if (node.depth > 4 && typeof node.value === 'undefined') {
+    // console.log('exploding node', print(node))
+
+    const index = flat.indexOf(node.left)
+
+    // console.log('from index', index, 'attempting explosing with flat', flat.map(n => n.value))
+
+    if (index) {
+      flat[index-1].value += node.left.value
+      // console.log('added left value', node.left.value, 'to left', flat[index-1]?.value)
+    }
+
+    if (flat.length > index + 2) {
+      flat[index+2].value += node.right.value
+      // console.log('added left value', node.right.value, 'to left', flat[index+2]?.value)
+    }
+
+    node.value = 0
+    node.left = undefined
+    node.right = undefined
+    return 'explode'
+  }
+
+  return explode(node.left, flat) || explode(node.right, flat)
+}
+
+
+// prints a flattened tree
+const print = node => JSON.stringify(flatten(node), null)
+
+// splits stuff
+const split = node => {
+  if (!node) return false
+
+  if (node.value > 9) {
+    // console.log('splitting node', print(node))
+    node.left = createNode(node.value/2|0, node, node.depth + 1)
+    node.right = createNode(-~node.value/2|0, node, node.depth + 1)
+    node.value = undefined
+
+    return 'split'
+  }
+
+  return split(node.left) || split(node.right)
+}
+
+export const reduce = (node, action, ticks = 0) => {
+  // console.log('reducing', print(node))
+  do {
+    ticks++
+    // const flat = values(node)
+
+    // const maxDepth = flat.reduce((acc, n) => Math.max(n.depth, acc), 0)
+    // const nodeToExplode = flat.find(n => n.depth > 4)?.parent
+    // const nodeToSplit = flat.find(n => n.value > 9)
+
+    // if (nodeToExplode) {
+    //   console.log('maxDepth of', print(node), 'is', maxDepth, 'should explode node', print(nodeToExplode))
+    //   action = explode(nodeToExplode, flat)
+    // } else if (nodeToSplit) {
+    //   console.log('attempting split instead', nodeToSplit)
+    //   action = split(nodeToSplit)
+    // }
+    // console.log(flat.map(n => ({ depth: n.depth, value: n.value })))
+
+    action = explode(node, values(node)) || split(node)
+    // if (action) {
+    //   console.log(action, print(node))
+    // }
+  } while (action)
+  return node
+}
+
+export const add = (node1, node2) => {
+  // console.log('add() node', print(node1), '+', print(node2))
+  for (const n of nodes(node1)) {
+    // console.log('updating depth of', n, 'from', n.depth, 'to', n.depth + 1)
+    n.depth++
+  }
+  for (const n of nodes(node2)) {
+    // console.log('updating depth of', n, 'from', n.depth, 'to', n.depth + 1)
+    n.depth++
+  }
+  return {
+    depth: 1,
+    left: node1,
+    right: node2,
+  }
+}
+
+export const doHomework = (input, part2) => {
+  const lines = typeof input === 'string' ? input.split('\n') : input //.map(line => JSON.parse(line)) : input
+
+  let root
+
+  for (const line of lines.map(line => JSON.parse(line))) {
+    // console.log(line, 'typeof', typeof line)
+    let node = createNode(line)
+    // console.log('adding node', print(node), 'to', root && print(root))
+    if (!root) {
+      root = node
+    } else {
+      root = reduce(add(root, node))
+    }
+  }
+
+  if (part2) {
+    let maxSum = -Infinity
+
+    // console.log(lines)
+
+    for (const l1 of lines) {
+      for (const l2 of lines) {
+        if (l1 !== 12) {
+          const b1a = createNode(JSON.parse(l1))
+          const b2a = createNode(JSON.parse(l2))
+          const b1b = createNode(JSON.parse(l1))
+          const b2b = createNode(JSON.parse(l2))
+          const pb1 = print(b1a)
+          const pb2 = print(b2a)
+          const sum1 = reduce(add(b1a, b2a))
+          const sum2 = reduce(add(b2b, b1b))
+          const mag1 = magnitude(sum1)
+          const mag2 = magnitude(sum2)
+
+
+
+          maxSum = Math.max(maxSum, mag1, mag2)
+
+          // console.log('adding', pb1, '+', pb2, 'yields', mag1, 'from', print(sum1), 'and', mag2, 'from', print(sum2))
+          // maxSum = Math.max(magnitude(add(b1, b2)), maxSum)
+          // maxSum = Math.max(magnitude(add(b2, b1)), maxSum)
+        }
+      }
+    }
+
+    return maxSum
+  }
+
+  // const flat = values(root)
+
+  // console.log(flat.map(n => ({ value: n.value, depth: n.depth, parentDepth: n.parent?.depth })))
+
+  // console.log('FINAL', print(root))
+
+  return root
 }
--- day 18/index.spec.js
+++ day 18/index.spec.js
@@ -1,19 +1,32 @@
-import * as challenge from '.'
+import { doHomework, magnitude } from '.'
 import actual from './input'
 
 // SAMPLE INPUT
-const sample1 = `[1,2]
-[[1,2],3]
-[9,[8,7]]
-[[1,9],[8,5]]
-[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
-[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
-[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]`
+
+const sample1 = `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]`
 
 describe('DAY 18', () => {
   describe('part 1', () => {
     it('should do a thing', () => {
-      expect(challenge.reduce([7,[6,[5,[4,[3,2]]]]])).toEqual([7,[6,[5,[7,0]]]])
+      const node = doHomework(sample1)
+
+      expect(magnitude(node)).toEqual(4140)
+    })
+
+    it('FINAL', () => {
+      // expect(challenge.reduce([7,[6,[5,[4,[3,2]]]]])).toEqual([7,[6,[5,[7,0]]]])
+      const node = doHomework(actual)
+
+      expect(magnitude(node)).toEqual(4641)
       // expect(challenge.reduce([[[[[9,8],1],2],3],4])).toEqual([[[[0,9],2],3],4])
     })
 
@@ -22,13 +35,13 @@ describe('DAY 18', () => {
     // })
   })
 
-  // describe('part 2', () => {
-  //   it('should do a thing', () => {
-  //     expect(challenge.run('target area: x=20..30, y=-10..-5', true)).toEqual(112)
-  //   })
+  describe('part 2', () => {
+    it('should do a thing with a different answer', () => {
+      expect(doHomework(sample1, true)).toEqual(3993)
+    })
 
-  //   it('FINAL', () => {
-  //     expect(challenge.run(actual, true)).toEqual(4716)
-  //   })
-  // })
+    it('FINAL', () => {
+      expect(doHomework(actual, true)).toEqual(3993)
+    })
+  })
 })
