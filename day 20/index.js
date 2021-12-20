661373853f048274c2b72eb2e77540520e8d9f67
Kevin Whitley <krwhitley@gmail.com>
2021-12-20 02:30:00 -0600

    cleanup day 20

--- day 20/index.js
+++ day 20/index.js
@@ -1,14 +1,14 @@
 import { range, sum } from 'supergeneric'
 
-// const copy = grid => JSON.parse(JSON.stringify(grid))
-
 const lit = grid => sum(Object.values(grid).map(line => Object.values(line)).flat())
 
+const bin2dec = bin => Number(parseInt(bin, 2).toString(10))
+
 const getBounds = grid => [
-    Math.min(...Object.keys(grid)),
-    Object.values(grid).reduce((acc, line) => Math.max(...Object.keys(line).map(Number), acc), -Infinity),
-    Math.max(...Object.keys(grid)),
-    Object.values(grid).reduce((acc, line) => Math.min(...Object.keys(line).map(Number), acc), Infinity),
+  Math.min(...Object.keys(grid)),
+  Object.values(grid).reduce((acc, line) => Math.max(...Object.keys(line).map(Number), acc), -Infinity),
+  Math.max(...Object.keys(grid)),
+  Object.values(grid).reduce((acc, line) => Math.min(...Object.keys(line).map(Number), acc), Infinity),
 ]
 
 const crop = (grid, bounds, steps) => {
@@ -25,43 +25,15 @@ const crop = (grid, bounds, steps) => {
   return g2
 }
 
-const print = grid => {
-  const top = Math.min(...Object.keys(grid))
-  const bottom = Math.max(...Object.keys(grid))
-  const left = Object.values(grid).reduce((acc, line) => Math.min(...Object.keys(line).map(Number), acc), Infinity)
-  const right = Object.values(grid).reduce((acc, line) => Math.max(...Object.keys(line).map(Number), acc), -Infinity)
-
-  let output = Array(bottom-top+1).fill('').map((line, li) => {
-    for (let x=left; x<=right; x++) {
-      line += grid[li+top][x] || '.'
-    }
-
-    return line
-  }).join('\n')
-
-  console.log(output)
-}
-
-// converts bits to decimal
-const bin2dec = bin => Number(parseInt(bin, 2).toString(10))
-
 const enhance = (grid, bounds, algo, step, inverse) => {
   const lut = {}
-
   const fill = Number(algo[0])
   const [top, right, bottom, left] = bounds
-
-  // const top = Math.min(...Object.keys(grid))
-  // const bottom = Math.max(...Object.keys(grid))
-  // const left = Object.values(grid).reduce((acc, line) => Math.min(...Object.keys(line).map(Number), acc), Infinity)
-  // const right = Object.values(grid).reduce((acc, line) => Math.max(...Object.keys(line).map(Number), acc), -Infinity)
   const g2 = {}
-
-  // console.log('processing from [top, right, bottom, left]', [top, right, bottom, left], 'with fill', fill)
   const spread = range(3, { from: -1 })
 
-  for (let l=top-100; l<bottom+100; l++) {
-    for (let c=left-100; c<right+100; c++) {
+  for (let l=top-70; l<bottom+70; l++) {
+    for (let c=left-70; c<right+70; c++) {
       let newChar
       const subgrid = spread.map(y => spread.map(x => grid[l+y]?.[c+x] || 0)).flat().join('')
 
@@ -74,8 +46,6 @@ const enhance = (grid, bounds, algo, step, inverse) => {
     }
   }
 
-  // print(g2)
-
   return g2
 }
 
@@ -85,17 +55,7 @@ export const process = (input, steps = 2) => {
   grid = grid
           .split('\n')
           .map(line => line.split('').map(Number))
-          .reduce((acc, line, li) => {
-            acc[li] = line.reduce((acc, c, ci) => {
-              if (c) {
-                acc[ci] = 1
-              }
-
-              return acc
-            },{})
-
-            return acc
-          }, {})
+          .reduce((acc, line, li) => (acc[li] = line.reduce((acc, c, ci) => c && (acc[ci] = 1) && acc || acc, {})) && acc || acc, {})
 
   const bounds = getBounds(grid)
 
@@ -105,7 +65,5 @@ export const process = (input, steps = 2) => {
 
   grid = crop(grid, bounds, steps)
 
-  // print(grid)
-
-  return lit(grid) //grid.flat().filter(v => v)
+  return lit(grid)
 }
