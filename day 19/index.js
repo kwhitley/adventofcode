f80729bb92aecb3034bf86480fbccbe9aa1e40a5
Kevin Whitley <krwhitley@gmail.com>
2021-12-19 21:40:28 -0600

    day 19 WIP

--- day 19/index.js
+++ day 19/index.js
@@ -1,4 +1,4 @@
-import { sum } from 'supergeneric'
+import { sum, average } from 'supergeneric'
 
 const distance = (p1, p2) => Math.sqrt(
   Object
@@ -13,7 +13,7 @@ const print = thing => JSON.stringify(thing, null)
 
 const getUnregistered = scanners => scanners.reduce((acc, scanner) => [...acc, ...scanner.filter(b => !b.registered)], [])
 
-const compareScanners = (s1, s2, set) => {
+const compareScanners = (s1, s2) => {
   // console.log('comparing scanners')
   const shared = []
 
@@ -23,17 +23,9 @@ const compareScanners = (s1, s2, set) => {
       if (intersection.length) {
 
         if (intersection.length >= 11) {
-          // set.add(a)
-          // shared.push(a)
-          set.add(a)
-          set.add(b)
-          // if (!a.registered) {
-          //   set.add(a)
-          // }
-
-          // if (!b.registered) {
-          //   set.add(b)
-          // }
+          a.intersection = intersection
+          b.intersection = intersection
+          shared.push([a, b])
           a.registered = true
           b.registered = true
         }
@@ -41,12 +33,7 @@ const compareScanners = (s1, s2, set) => {
     }
   }
 
-  // if (shared.length >= 12) {
-  //   console.log('shared length is', shared.length)
-  // }
   return shared
-
-  // console.log('found', shared.length, 'shared points') //, shared.map(s => s[0]))
 }
 
 export const run = (input, part2) => {
@@ -69,38 +56,122 @@ export const run = (input, part2) => {
         signal.distances.push(d)
       }
 
-      signal.distanceSum = sum(signal.distances)
+      // signal.distanceSum = sum(signal.distances)
       // unique.add(signal.distanceSum)
 
       // console.log('signal', print(signal), 'distances', signal.distances)
     }
   }
 
+  scanners[0].position = [0,0,0]
+
+  const identified = new Set([scanners[0]])
+  const unidentified = new Set(scanners.slice(1))
   const beacons = new Set()
 
-  for (let a=0; a<scanners.length; a++) {
-    for (let b=0; b<scanners.length; b++) {
-      if (a === b) continue
+  let ticks = 0
 
-      const shared = compareScanners(scanners[a], scanners[b], beacons)
-      // beacons = [...beacons, ...shared]
 
-      if (shared.length) {
-        // console.log('found', shared.length, 'shared beacons')
+  next: while (ticks < 100 && unidentified.size) {
+    ticks++
+    for (const a of unidentified) {
+      for (const b of identified) {
+        if (a === b) continue
+
+        const shared = compareScanners(a, b)
+
+
+        if (shared.length) {
+          console.log('matched scanner', scanners.indexOf(a), 'with scanner', scanners.indexOf(b))
+          console.log('removing scanner', scanners.indexOf(a))
+
+          // console.log('SCANNER A', a.map(s => s.distances))
+          // console.log('SCANNER B', b.map(s => s.distances))
+
+          console.log('SHARED SIGNALS', shared[0])
+          const [signalA, signalB] = shared[0]
+          // const [s]
+          // const ratios = [share]
+
+
+
+
+
+          console.log('breaking')
+          break next
+
+          const centerA = [
+            average(shared.map(s => s[0][0])),
+            average(shared.map(s => s[0][1])),
+            average(shared.map(s => s[0][2])),
+          ]
+          const centerB = [
+            average(shared.map(s => s[1][0])),
+            average(shared.map(s => s[1][1])),
+            average(shared.map(s => s[1][2])),
+          ]
+
+          const offset = centerA.map((a, i) => a + centerB[i])
+
+          unidentified.delete(a)
+          identified.add(a)
+          shared.forEach(beacon => beacons.add(beacon))
+          // b.forEach(beacon => {
+          //   if (!shared.includes(beacon)) {
+          //     beacons.add(beacon)
+          //   }
+          // })
+          console.log('centerA', centerA, 'and centerB', centerB, 'with offset', offset)
+
+          const translated = b.map(coords => coords.map((v, i) => v + offset[i]))
+
+          console.log('scannerA', print(a).replace(/],/g, '\n'))
+          console.log('scannerB', print(b).replace(/],/g, '\n'))
+          console.log('translated', print(translated).replace(/],/g, '\n'))
+          console.log('there are now', identified.size, 'identified and', unidentified.size, 'unidentified scanners')
+        }
       }
     }
+    //     if (a === b) continue
+
+    //     const shared = compareScanners(scanners[a], scanners[b], beacons)
+    //     // beacons = [...beacons, ...shared]
+
+    //     if (shared.length) {
+    //       // console.log('found', shared.length, 'shared beacons')
+    //     }
+    //   }
 
   }
 
-  const unregistered = getUnregistered(scanners)
-  console.log(unregistered.length, 'unregistered beacons')
+  console.log('scanners[0].length', scanners[0].length, 'and', scanners.length, 'total scanners')
+  console.log('beacons logged', beacons.size)
 
-  // return beacons.size
+  // const beacons = new Set()
 
-  const scannerMath = beacons.size + unregistered.length - scanners.length - 1
+  // for (let a=0; a<scanners.length; a++) {
+  //   for (let b=0; b<scanners.length; b++) {
+  //     if (a === b) continue
 
-  console.log('beacons length is', beacons.size)
-  console.log('scanner math =', scannerMath)
+  //     const shared = compareScanners(scanners[a], scanners[b], beacons)
+  //     // beacons = [...beacons, ...shared]
+
+  //     if (shared.length) {
+  //       // console.log('found', shared.length, 'shared beacons')
+  //     }
+  //   }
+
+  // }
+
+  // const unregistered = getUnregistered(scanners)
+  // console.log(unregistered.length, 'unregistered beacons')
+
+  // // return beacons.size
+
+  // const scannerMath = beacons.size + unregistered.length - scanners.length - 1
+
+  // console.log('beacons length is', beacons.size)
+  // console.log('scanner math =', scannerMath)
 
 
   // const scanner0 = scanners[0].map(s => s.distanceSum)
